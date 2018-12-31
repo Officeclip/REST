9f5349b61ec6761b2ea85013df3c5e4761e4d315
SK Dutta <skd@officeclip.com>
2018-12-31 18:12:11 -0500

    added the nuget specs

--- Rest.Library/Properties/AssemblyInfo.cs
+++ Rest.Library/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Security;
 [assembly: AssemblyTitle("Rest.Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("OfficeClip")]
 [assembly: AssemblyProduct("Rest.Library")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -33,7 +33,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
 
 [assembly: AllowPartiallyTrustedCallersAttribute()]
